fcb4c900b821fb9af5616756450302e52fce5b14
Egor Geller <egkingers11@gmail.com>
2024-03-16 17:30:11 +0200

    New Questions

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ def questions():
             else:
                 progress_bar = 0
                 return render_template('questions_0.html', progress_bar=progress_bar, question_number=0)
-        elif 'disagree' in request.form:
+        if 'disagree' in request.form:
             return redirect('/')
-    elif request.method == 'GET':
+    if request.method == 'GET':
         return redirect('/')
 
 
@@ -36,49 +36,68 @@ def answer():
             template_name = 'questions_' + str(quest_num + 1) + '.html'
             progress_bar = (quest_num + 1) / 10 * 100
             return render_template(template_name, progress_bar=progress_bar, question_number=quest_num + 1)
+        if 'next_end' in request.form:
+            return finish(answers)
+        if 'back' in request.form:
+            quest_num = int(request.form['question_number'])
+            if quest_num <= 0:
+                return redirect("/")
+            template_name = 'questions_' + str(quest_num - 1) + '.html'
+            progress_bar = (quest_num - 1) / 10 * 100
+            return render_template(template_name, progress_bar=progress_bar, question_number=quest_num - 1)
 
+def finish(data):
+    return render_template('Finish.html', data=data)
 
 def save_data(form):
     if 'user_name' in form:
         answers['name'] = form['user_name']
-    elif 'age_category' in form:
+    if 'age_category' in form:
         answers['age_category'] = form['age_category']
-    elif 'sex' in form:
+    if 'sex' in form:
         answers['sex'] = form['sex']
-    elif 'height' in form:
+    if 'height' in form:
         answers['height'] = form['height']
-    elif 'smoke_yes' in form:
+    if 'smoke_yes' in form:
         answers['smoke'] = form['smoke_yes']
-    elif 'smoke_no' in form:
+    if 'smoke_no' in form:
         answers['smoke'] = form['smoke_no']
-    elif 'general_health' in form:
+    if 'general_health' in form:
         answers['general_health'] = form['general_health']
-    elif 'doctor_visit' in form:
+    if 'doctor_visit' in form:
         answers['doctor_visit'] = form['doctor_visit']
-    elif 'exercise_yes' in form:
+    if 'exercise_yes' in form:
         answers['exercise_yes'] = form['exercise_yes']
-    elif 'exercise_no' in form:
+    if 'exercise_no' in form:
         answers['exercise_no'] = form['exercise_no']
-    elif 'depressive_disorder_yes' in form:
+    if 'depressive_disorder_yes' in form:
         answers['depressive_disorder_yes'] = form['depressive_disorder_yes']
-    elif 'depressive_disorder_no' in form:
+    if 'depressive_disorder_no' in form:
         answers['depressive_disorder_no'] = form['depressive_disorder_no']
-    elif 'diabetes_yes' in form:
+    if 'diabetes_yes' in form:
         answers['diabetes_yes'] = form['diabetes_yes']
-    elif 'diabetes_no' in form:
+    if 'diabetes_no' in form:
         answers['diabetes_no'] = form['diabetes_no']
-    elif 'arthritis_yes' in form:
+    if 'arthritis_yes' in form:
         answers['arthritis_yes'] = form['arthritis_yes']
-    elif 'arthritis_no' in form:
+    if 'arthritis_no' in form:
         answers['arthritis_no'] = form['arthritis_no']
-    elif 'skin_cancer_yes' in form:
+    if 'skin_cancer_yes' in form:
         answers['skin_cancer_yes'] = form['skin_cancer_yes']
-    elif 'skin_cancer_no' in form:
+    if 'skin_cancer_no' in form:
         answers['skin_cancer_no'] = form['skin_cancer_no']
-    elif 'other_cancer_yes' in form:
+    if 'other_cancer_yes' in form:
         answers['other_cancer_yes'] = form['other_cancer_yes']
-    elif 'other_cancer_no' in form:
+    if 'other_cancer_no' in form:
         answers['other_cancer_no'] = form['other_cancer_no']
+    if 'drink_range' in form:
+        answers['drink_range'] = form['drink_range']
+    if 'eat_fruit' in form:
+        answers['eat_fruit'] = form['eat_fruit']
+    if 'veggies' in form:
+        answers['veggies'] = form['veggies']
+    if 'fried' in form:
+        answers['fried'] = form['fried']
 
     print(answers)
 
